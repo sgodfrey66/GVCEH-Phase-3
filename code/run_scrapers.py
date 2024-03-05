2db610ed9a6be30962c998ad81cca3264c988c31
sgodfrey66 <stephen_godfrey@ymail.com>
2024-03-05 15:59:15 -0800

    workflow

--- code/run_scrapers.py
+++ code/run_scrapers.py
@@ -8,12 +8,12 @@ import dotenv
 import asyncio
 
 # GVCEH objects
-sys.path.insert(0, 'reddit/')
+sys.path.insert(0, "code/reddit/")
 import reddit_data_fetcher as rdf
 
 
 # GVCEH objects
-sys.path.insert(0, 'xtwitter/')
+sys.path.insert(0, "code/xtwitter/")
 import x_twitter_data_fetcher as xtdf
 
 
@@ -34,12 +34,17 @@ if __name__ == "__main__":
     # Step 1: Initialize GVCEHReddit object
     data_fetcher = rdf.GVCEHReddit(client_id=os.environ.get("REDDIT_CLIENT_ID"),
                                    client_secret=os.environ.get("REDDIT_CLIENT_SECRET"),
-                                   user_agent=os.environ.get("REDDIT_USER_AGENT"))
+                                   user_agent=os.environ.get("REDDIT_USER_AGENT"),
+                                   posts_file_path="data/reddit/posts",
+                                   logs_file_path = "data/reddit/logs",
+                                   keywords_file_path = "data/keywords")
 
     # Subreddits to explore
-    subreddit_names = ['OakBayBritishColumbia', 'SaanichPeninsula', 'britishcolumbia',
-                       'Sooke', 'Esquimalt', 'SidneyBC', 'saltspring',
-                       'Metchosin', 'WestShoreBC', 'VancouverIsland', 'uvic']
+    subreddit_names = ["OakBayBritishColumbia", "SaanichPeninsula", "britishcolumbia",
+                       "Sooke", "Esquimalt", "SidneyBC", "saltspring",
+                       "Metchosin", "WestShoreBC", "VancouverIsland", "uvic"]
+
+    subreddit_names = ['britishcolumbia']
 
 
     # Step 2: Fetch reddit data
@@ -48,14 +53,14 @@ if __name__ == "__main__":
 
 
     # Step 3: Initialize GVCEHXTwitter object
-    data_fetcher = xtdf.GVCEHXTwitter(bearer_token=os.environ.get("TWITTER_BEARER_TOKEN"),
-                                      consumer_key=os.environ.get("TWITTER_CONSUMER_KEY"),
-                                      consumer_secret=os.environ.get("TWITTER_CONSUMER_SECRET"),
-                                      access_token=os.environ.get("TWITTER_ACCESS_TOKEN"),
-                                      access_token_secret=os.environ.get("TWITTER_ACCESS_TOKEN_SECRET"))
+    # data_fetcher = xtdf.GVCEHXTwitter(bearer_token=os.environ.get("TWITTER_BEARER_TOKEN"),
+    #                                   consumer_key=os.environ.get("TWITTER_CONSUMER_KEY"),
+    #                                   consumer_secret=os.environ.get("TWITTER_CONSUMER_SECRET"),
+    #                                   access_token=os.environ.get("TWITTER_ACCESS_TOKEN"),
+    #                                   access_token_secret=os.environ.get("TWITTER_ACCESS_TOKEN_SECRET"))
 
     # Step 4: Fetch Twitter data
-    data_fetcher.batch_scrape()
+    # data_fetcher.batch_scrape()
 
     print('Scrapers run complete')
 
