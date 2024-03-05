7f0744ed497b9ca4746034065bebf62bd4fa8887
sgodfrey66 <stephen_godfrey@ymail.com>
2024-03-05 15:13:16 -0800

    workflow

---
+++ code/test_ga.py
@@ -0,0 +1,32 @@
+# Core Python
+import os, sys
+
+
+# # Environment variables for API credential storage
+# import dotenv
+#
+# # Asynchronous work
+# import asyncio
+#
+# # GVCEH objects
+# sys.path.insert(0, 'reddit/')
+# import reddit_data_fetcher as rdf
+#
+# # GVCEH objects
+# sys.path.insert(0, 'xtwitter/')
+# import x_twitter_data_fetcher as xtdf
+
+if __name__ == "__main__":
+    '''
+    This function uses the GVCEHReddit class to retrieve 
+    Reddit from specific Subreddits by searching for specific 
+    keyword or search terms.
+
+    See GVCEHReddit documentation for details on inputs and outputs.
+
+    '''
+
+    print(os.listdir('../'))
+    print(os.listdir('../data/reddit/posts'))
+
+    print('hello')
